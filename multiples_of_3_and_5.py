90ed460e6d5d50ae0bbc433d806eb57813254003
srikar1993 <32321399+srikar1993@users.noreply.github.com>
2021-11-21 22:34:40 +0530

    Update multiples_of_3_and_5.py

--- multiples_of_3_and_5.py
+++ multiples_of_3_and_5.py
@@ -1,11 +1,23 @@
 import sys
 
+# Test Cases
+t = int(input().strip())
 
-t = int(input().strip()) # Test Cases
 for i in range(t):
-    n = int(input().strip()) # If n = 10
-    three = (n - 1) // 3 # three = 3
-    five = (n - 1) // 5 # five = 1
-    fifteen = (n - 1) // 15 # fifteen = 0
-    sum = 3 * (three * (three + 1) // 2) + 5 * (five * (five + 1) // 2) - 15 * (fifteen * (fifteen + 1) //2) # sum = (3 * 6) + (5 * 1) - (0) => sum = 18 + 5 => sum = 23
+    
+    # If n = 10
+    n = int(input().strip())
+    
+    # three = 3
+    three = (n - 1) // 3
+    
+    # five = 1
+    five = (n - 1) // 5
+    
+    # fifteen = 0
+    fifteen = (n - 1) // 15
+    
+    # sum = (3 * 6) + (5 * 1) - (0) => sum = 18 + 5 => sum = 23
+    sum = 3 * (three * (three + 1) // 2) + 5 * (five * (five + 1) // 2) - 15 * (fifteen * (fifteen + 1) //2)
+    
     print(sum)
